5b1652075c4889d30f59f7133de3d7d2cf8db8ac
Rawad-Yakub <yakubrawad@gmail.com>
2023-10-09 21:06:46 -0400

    Revert "FillU implemented"
    This reverts commit 74fa8ee54e95f664cc1f8fbcd082757492274588.

--- script.js
+++ script.js
@@ -1,45 +1,108 @@
 // Declare global variables
 let numRows = 0;
 let numCols = 0;
-let colorSelected; 
+let colorSelected;
 
-// Add a row
-function addR() {
-    alert("Clicked Add Row"); // Replace this line with your code.
+
+
+function addR() { // Add a row
+    var Table = document.getElementById('grid');
+
+    if (Table.rows.length == 0) {
+        var newR = Table.insertRow();
+
+        var newCell = newR.insertCell();
+
+        newCell.className = 'cell';
+        newCell.addEventListener('click', function() { changeColor(newCell) });
+    } 
+    
+    else {
+        // makes first row + create a new 
+        var originRow = Table.rows[0];
+        
+            var newR = originRow.cloneNode(true);
+
+        // clears background color of cells in new row
+        var ColorCell = newR.querySelectorAll('.cell');
+        
+            for (var i = 0; i < ColorCell.length; i++) {
+                
+                ColorCell[i].style.backgroundColor = '';
+            }
+
+        // Adds new row to the table
+        Table.appendChild(newR);
+
+    }
 }
+
+
 
 // Add a column
 function addC() {
-    alert("Clicked Add Col"); // Replace this line with your code.
+    const grid = document.getElementById("grid");
+
+    for (let i = 0; i < grid.rows.length; i++) {
+        const cell = grid.rows[i].insertCell(-1);
+        cell.onclick = function () {
+            colorSingleCell(this);
+        };
+    }
+    numCols++;
 }
 
 // Remove a row
 function removeR() {
-    alert("Clicked Remove Row"); // Replace this line with your code.
+    
+    var grid = document.getElementById('grid');
+
+    var LastR = grid.rows[grid.rows.length - 1];
+    grid.deleteRow(grid.rows.length - 1);
 }
 
 // Remove a column
 function removeC() {
-    alert("Clicked Remove Col"); // Replace this line with your code.
+    const grid = document.getElementById("grid");
+	if (numCols > 0) {
+		for(let i = 0; i < grid.rows.length;i++) {
+			grid.rows[i].deleteCell(-1); 
+		}
+		numCols--;
+	}
 }
 
 // Set global variable for selected color
 function selectColor(){
     colorSelected = document.getElementById("selectedColorId").value;
-    console.log(colorSelected);
 }
 
 // Fill all uncolored cells
 function fillU(){
-    alert("Clicked Fill All Uncolored"); // Replace this line with your code.
+    
 }
 
 // Fill all cells
 function fillAll(){
-    alert("Clicked Fill All"); // Replace this line with your code.
+    if (colorSelected !== "SELECT") { // Check if a color is selected
+        const gridCells = document.querySelectorAll('td');
+
+        gridCells.forEach(function(cell) {
+            cell.style.backgroundColor = colorSelected;
+        });
+    }
+    
 }
 
 // Clear all cells
 function clearAll(){
-    alert("Clicked Clear All"); // Replace this line with your code.
+  const gridCells = document.querySelectorAll("td");
+  gridCells.forEach(function(cell){
+    cell.style.backgroundColor = "white"
+  });
+}
+
+// Fill single cell
+function colorSingleCell(element){
+  element.style.backgroundColor = colorSelected;
 }
